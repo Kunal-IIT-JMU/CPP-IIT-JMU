030abed8ddfd6d85b363cbc883640b1b6467bb4d
kunal-coder-02 <159030119+kunal-coder-02@users.noreply.github.com>
2024-02-07 21:54:04 +0530

    ok

---
+++ 9.Binary to Deciaml.cpp
@@ -0,0 +1,27 @@
+//Kunal Sharma IIT JAMMU
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//function to convert to binary
+int binary_to_decimal(int n){
+    int ans = 0;
+    int i =0;
+    while(n>0){
+        int y = n % 10;
+        ans = ans + y*pow(2,i);
+        i ++;
+        n = n/10;
+    }
+    return ans;
+}
+
+int main(){
+
+    int n; 
+    cin >> n;
+
+    cout << binary_to_decimal(n);
+
+    return 0;
+}
