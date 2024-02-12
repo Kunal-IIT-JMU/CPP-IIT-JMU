986f91bd93501bb92cb6187cbb4a5663391939c5
Kunal Sharma <159030119+Kunal-IIT-JMU@users.noreply.github.com>
2024-02-12 11:48:08 +0000

    ok

--- _17.String Problems.cpp
+++ _17.String Problems.cpp
@@ -3,7 +3,30 @@
 
 using namespace std;
 
+//Creating functions to uppercase the given string
+string uppercase(string s){
+    for(int i = 0 ; i < s.size(); i++){
+        if(s[i] >= 'a' && s[i] <= 'z'){
+            s[i] = s[i] - 32;
+        }
+    }
+    return s;
+}
+
+//Creating a Function to lowercase the given string
+string lowercase(string s){
+    for(int i = 0 ; i < s.size(); i++){
+        if(s[i] >= 'A' && s[i] <= 'Z'){
+            s[i] = s[i] + 32;
+        }
+    }
+    return s;
+}
 int main(){
+
+    string s = "Kunal Sharma";
+    cout << uppercase(s) << endl;
+    cout << lowercase(s) << endl;
     
     return 0;
 }
