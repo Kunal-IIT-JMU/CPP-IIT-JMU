d603e5d99e040cba61567f166fce4971de86bebb
Kunal Sharma <159030119+Kunal-IIT-JMU@users.noreply.github.com>
2024-02-12 15:45:15 +0000

    ok

--- _17.String Problems.cpp
+++ _17.String Problems.cpp
@@ -37,5 +37,34 @@ int main(){
     transform(x.begin(), x.end() ,x.begin(), ::tolower);        //return LOWERCASE VERSION
     cout << x << endl;
 
+    //find the greatest number from the given string
+    string num = "1592364780";
+    sort(num.begin(),num.end());
+    reverse(num.begin(),num.end());
+    cout << num << endl;
+
+    //finding the frequency of the alphabets in the string
+    string st = "fajdkhfdslfhaoifd";
+
+    int f[26];
+
+    for(int i =0; i<26; i++)     //making an array to store the frequency of the array parts
+        f[i] = 0;
+    
+    for(int i =0; i<26; i++){   //stroring the freq at their place in the array
+        f[st[i]-'a'] ++;
+    }
+
+    char ans = 'a';
+    int maxF = 0;
+    
+    for(int i =0; i<26; i++){   //finding the maximum of the array and the character which has max frequency
+        if(f[i] > maxF){
+            maxF = f[i];
+            ans = i + 'a';
+        }
+    }
+
+    cout << maxF << " " << ans << endl ;    //printing the answer
     return 0;
 }
