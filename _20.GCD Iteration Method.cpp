eaf87f53eec13f2eb52eb72ee62b6c44647089a4
Kunal Sharma <159030119+Kunal-IIT-JMU@users.noreply.github.com>
2024-02-13 13:59:11 +0000

    ok

--- _20.GCD Iteration Method.cpp
+++ _20.GCD Iteration Method.cpp
@@ -3,7 +3,33 @@
 
 using namespace std;
 
+//Code for GCD
+int GCD(int a, int b){
+    if(a==0)
+    return b;
+
+    if(b == 0)
+    return a;
+
+    while (a!=b){
+        if(a>b){
+            a = a-b;
+        }
+        else{
+            b = b-a;
+        }
+    }
+    return a;
+}
+
 int main(){
     
+    int a,b;
+    cout << "Enter the Values of a and b" << endl;
+    cin >> a >> b;
+
+    int ans = GCD(a, b);
+    cout << ans << endl;
+
     return 0;
 }
