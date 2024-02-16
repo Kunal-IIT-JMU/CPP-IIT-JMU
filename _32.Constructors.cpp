7d8320af69aa8ab8bb62322ce8461971a84cdbba
Kunal Sharma <159030119+Kunal-IIT-JMU@users.noreply.github.com>
2024-02-16 17:44:03 +0000

    ok

--- _32.Constructors.cpp
+++ _32.Constructors.cpp
@@ -20,7 +20,7 @@ class student{
         gender = g;
     }
 
-    student(student &a){
+    student(student &a){                //copy constuctor (Deep Copy)
         name = a.name;
         age = a.age;
         gender = a.gender;
@@ -31,6 +31,17 @@ class student{
         cout << age << endl;
         cout << gender << endl;
     }
+
+    ~student(){                        //Destructer to erase the memory
+        cout << "Destructor called !!!" << endl;        //it will be called as many times as the no of objects created
+    }
+
+    bool operator == (student &a){     //operator overloading
+        if(name == a.name && age == a.age && gender == a.gender){       //used to make a n operator behave as we want i.e. predefine the output of the operator
+            return true;
+        }
+        return false;
+    }
 };
 
 int main(){
@@ -38,10 +49,16 @@ int main(){
     student s1("Kunal Sharma",18,"Male");
     s1.getinfo();
 
-    cout << "*******************************" << endl;
+    cout << endl;
 
     student b = s1;
     b.getinfo();
 
+    cout << endl;
+
+    if(b == s1)
+        cout << "Same" << endl;
+    else
+        cout << "Not Same" << endl ;
     return 0;
 }
