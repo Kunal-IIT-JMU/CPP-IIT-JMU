8f3f65ef77b794f38feeb744614faf09ca3e4c53
Kunal Sharma <159030119+Kunal-IIT-JMU@users.noreply.github.com>
2024-02-17 09:55:00 +0000

    ok

--- _37.Linked Lists L1.cpp
+++ _37.Linked Lists L1.cpp
@@ -10,32 +10,49 @@ class node
     int data;
     node* next;
 
-    node (int val){
+    node (int val)
+    {
         data = val;
         next = NULL;
     }
-
-    void insert(node * &head, int val){
-        node * n = new node(val);
-
-        node * temp = head;
-        while(temp -> next != NULL){
-            temp = temp -> next;
-        }
-        temp -> next = n;
-        return ;
-    }
-
-    void display(node* head){
-        node * temp = head;
-        while (temp != NULL){
-            cout << temp -> data << " ";
-            temp = temp -> next;
-        }
-        return ;
-    }
-
 };
+
+void insert(node* &head, int val){
+    node* n = new node(val);
+
+    if(head == NULL){
+        head = n;
+        return ;
+    }
+
+    node* temp = head;
+    while(temp -> next != NULL){
+        temp = temp -> next;
+    }
+    temp -> next = n;
+    return ;
+}
+
+void display(node* head){
+    node* temp = head;
+    while (temp != NULL){
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout << endl;
+    return ;
+}
+
+bool search (node* head , int key){
+    node* temp = head;
+    while (temp != NULL){
+        if(temp->data == key){
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
 
 int main(){
 
@@ -44,6 +61,7 @@ int main(){
     insert(head ,2);
     insert(head ,3);
     display(head);
+    cout << search(head,2) << endl;
 
     return 0;
 }
