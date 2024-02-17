97ee4ade19f54429a91631d6d703992ce0ac39da
Kunal Sharma <159030119+Kunal-IIT-JMU@users.noreply.github.com>
2024-02-17 10:27:59 +0000

    ok

--- _37.Linked Lists L1.cpp
+++ _37.Linked Lists L1.cpp
@@ -39,8 +39,8 @@ void insert(node* &head, int val){
 void display(node* head){
     node* temp = head;
     while (temp != NULL){
-        cout << temp -> data << " ";
-        temp = temp -> next;
+        cout << temp -> data << " ";    //printing the data in the linked lists
+        temp = temp -> next;            //iterating to the next element of the linked list
     }
     cout << endl;
     return ;
@@ -50,12 +50,23 @@ void display(node* head){
 bool search (node* head , int key){
     node* temp = head;
     while (temp != NULL){
-        if(temp->data == key){
+        if(temp->data == key){          //checking if key is in linked list or not
             return true;
         }
         temp = temp->next;
     }
     return false;
+}
+
+//Code for deleting a node in the linked lists
+void Delete(node* &head, int val){
+    node* temp = head;
+    while(temp -> next -> data != val){     //checking if the data at the next node is equal to the key
+        temp = temp -> next;                //iterating through the linked lists
+    }
+    node* todelete = temp->next;            
+    temp -> next = temp -> next -> next;
+    delete todelete;                        //deleting the given node
 }
 
 int main(){
@@ -64,7 +75,10 @@ int main(){
     insert(head ,1);    //entering first element in the linked lists
     insert(head ,2);    //entering second element in the linked lists
     insert(head ,3);    //entering third element in the linked lists
+    insert(head ,4);    //entering fourth element in the linked lists
     display(head);      //printing the linked list
+    Delete(head ,2);    //Deleting the node in the linked lists
+    display(head);
     cout << search(head,2) << endl;     //searching 2 in the linked list will give 1 if present else 0
 
     return 0;
