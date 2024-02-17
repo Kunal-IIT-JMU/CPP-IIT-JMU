bd8d23f5178b93d61975f8870197ccb628571d90
Kunal Sharma <159030119+Kunal-IIT-JMU@users.noreply.github.com>
2024-02-17 15:00:30 +0000

    ok

--- _38.Reverse a Linked Lists.cpp
+++ _38.Reverse a Linked Lists.cpp
@@ -17,7 +17,54 @@ class node
     }
 };
 
-int main(){
+node* reverse(node* &head){
+    node* prevptr = NULL;
+    node* currptr = head;
+    node* nextptr ;
 
+    while(currptr != NULL){
+        nextptr = currptr -> next;
+        currptr -> next = prevptr;
+        prevptr = currptr;
+        currptr = nextptr;
+    }
+    return prevptr;
+}
+
+void insert(node* &head, int val){
+    node* n = new node(val);            
+
+    if(head == NULL){                   
+        head = n;
+        return;}
+
+    node* temp = head;                  
+    while(temp -> next != NULL){
+        temp = temp -> next;
+    }
+    temp -> next = n;                   
+    return ;
+}
+
+void display(node* head){
+    node* temp = head;
+    while (temp != NULL){
+        cout << temp -> data << " ";    
+        temp = temp -> next;            
+    }
+    cout << endl;
+    return ;
+}
+
+
+int main(){
+    node* head = NULL;
+    insert(head,1);
+    insert(head,2);
+    insert(head,3);
+    insert(head,4);
+    display(head);
+    node* newhead = reverse(head);
+    display(newhead);
     return 0;
 }
