71d2a07d22c213f79da70ac8e9a185c6973b1a73
Kunal Sharma <159030119+Kunal-IIT-JMU@users.noreply.github.com>
2024-02-17 10:13:15 +0000

    ok

--- _37.Linked Lists L1.cpp
+++ _37.Linked Lists L1.cpp
@@ -3,36 +3,39 @@
 
 using namespace std;
 
-//linked list node class
+//linked list code for creating a linked list
 class node
 {
     public:
-    int data;
-    node* next;
+    int data;       //creating a variable in the node class to store the data
+    node* next;     //creating a variable in the node class to store address of next node
 
-    node (int val)
+    node (int val)  //creating a constructer to put values of data and address of NULL in the variables
     {
         data = val;
         next = NULL;
     }
 };
+//this will create a linked list with one node only
 
+//code to insert an element in the end of the linked lists
 void insert(node* &head, int val){
-    node* n = new node(val);
+    node* n = new node(val);            //creating a new node to add in the linked lists
 
-    if(head == NULL){
+    if(head == NULL){                   //adding the address of the next node in the prev
         head = n;
-        return ;
+        return;
     }
 
-    node* temp = head;
+    node* temp = head;                  //iterate through the linked lists to reach the end
     while(temp -> next != NULL){
         temp = temp -> next;
     }
-    temp -> next = n;
+    temp -> next = n;                   //when it reaches the end of the linked list replace the address of the new element 
     return ;
 }
 
+//code for printing the linked list
 void display(node* head){
     node* temp = head;
     while (temp != NULL){
@@ -43,6 +46,7 @@ void display(node* head){
     return ;
 }
 
+//code for searching an element in the linked lists
 bool search (node* head , int key){
     node* temp = head;
     while (temp != NULL){
@@ -57,11 +61,11 @@ bool search (node* head , int key){
 int main(){
 
     node * head = NULL;
-    insert(head ,1);
-    insert(head ,2);
-    insert(head ,3);
-    display(head);
-    cout << search(head,2) << endl;
+    insert(head ,1);    //entering first element in the linked lists
+    insert(head ,2);    //entering second element in the linked lists
+    insert(head ,3);    //entering third element in the linked lists
+    display(head);      //printing the linked list
+    cout << search(head,2) << endl;     //searching 2 in the linked list will give 1 if present else 0
 
     return 0;
 }
